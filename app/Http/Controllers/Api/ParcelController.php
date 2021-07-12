16e29f2fb526fd31b57b0a17d71f700e618bfdf0
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2021-07-12 09:50:47 -0400

    fix parcel
    enabled and disabled

--- app/Http/Controllers/Api/ParcelController.php
+++ app/Http/Controllers/Api/ParcelController.php
@@ -116,4 +116,22 @@ class ParcelController extends Controller
         return response()->json(['ok' => true, 'message' => ' se elimino exitosamente'], 200);
     }
 
+    public function enabled($id)
+    {
+        try {
+            $parcel = Parcel::findOrFail($id);
+
+            if ($parcel->enabled == true) {
+                $parcel->enabled = false;
+                $parcel->save();
+                return response()->json(['ok' => true, 'message' => 'Parcel inactivo'], 201);
+            } else {
+                $parcel->enabled = true;
+                $parcel->save();
+                return response()->json(['ok' => true, 'message' => 'Parcel activo'], 201);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['ok' => false, 'message' => 'Parcel not found!', 'error' => $e], 404);
+        }
+    }
 }
