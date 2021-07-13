f2cb32d6d43f9e1ac3c8e7a8c391cd1859b30149
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2021-07-13 01:11:46 -0400

    fix rol

--- app/Http/Controllers/Api/RolController.php
+++ app/Http/Controllers/Api/RolController.php
@@ -96,5 +96,32 @@ class RolController extends Controller
     public function destroy($id)
     {
         //
+        //
+        $Rol = Rol::FindOrFail($id);
+        if ($Rol) {
+            $Rol->delete();
+        } else {
+            return response()->json(['ok' => false, 'message' => 'Error does not exist Rol'], 409);
+        }
+        return response()->json(['ok' => true, 'message' => ' se elimino exitosamente'], 200);
+
+    }
+    public function enabled($id)
+    {
+        try {
+            $rol = Rol::findOrFail($id);
+
+            if ($rol->enabled == true) {
+                $rol->enabled = false;
+                $rol->save();
+                return response()->json(['ok' => true, 'message' => 'rol inactivo'], 201);
+            } else {
+                $rol->enabled = true;
+                $rol->save();
+                return response()->json(['ok' => true, 'message' => 'rol activo'], 201);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['ok' => false, 'message' => 'Rol not found!', 'error' => $e], 404);
+        }
     }
 }
