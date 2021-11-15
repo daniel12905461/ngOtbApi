34b881e63a49263e35e72ffd3c94c94186a35ba4
eberKachi <eber.nina96@gmail.com>
2021-11-14 22:25:59 -0400

    create tabla member_parcel

---
+++ database/migrations/2021_07_12_000001_create_member_parcel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberParcelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_parcel', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('parcel_id');
+            $table->foreign('parcel_id')->references('id')->on('parcels')->onDelete('cascade');
+
+            $table->unsignedBigInteger('member_id');
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_parcel');
+    }
+}
